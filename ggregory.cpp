a0123279668ca09a7b9282f4a40dbd58f4319b8a
Garrett Gregory <ggregory@odin.cs.csubak.edu>
2025-03-11 08:51:50 -0700

    added name to screen

---
+++ ggregory.cpp
@@ -0,0 +1,12 @@
+// Original author: Garrett Gregory
+// Date: March 11, 2025
+
+#include <cstdio>
+#include "ggregory.h"
+
+void garrettText(Rect *rGarrett)
+{
+    ggprint8b(rGarrett, 16, 0xffffff, "Greg");
+}
+
+//create the game over screen (click g and the scene will be presented)
