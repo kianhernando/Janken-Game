bee84bb429826016b0012a9ead3a3907459a2561
Simon Santos <shinkuu03@gmail.com>
2025-03-11 08:02:18 -0700

    added name to the top right corner

--- ssantos.cpp
+++ ssantos.cpp
@@ -1,6 +1,5 @@
 // Original author: Simon Santos
 // Date: March 08, 2025
-// pp pupu
 #include <cstdio>
 #include "ssantos.h"
 
@@ -11,6 +10,5 @@ void testFunction()
 
 void simonText(Rect *rSimon)
 {
-    ggprint8b(rSimon, 16, 0xffffff, "Hello");
-    ggprint8b(rSimon, 16, 0xffffff, "World");
+    ggprint8b(rSimon, 16, 0xffffff, "Simon");
 }
